ae80f72b82cc4587a0320c331a08eadd4db27ac1
panhdjf <phuonganh080701a3@gmai.com>
2022-10-28 16:25:38 +0700

    A

--- test.go
+++ test.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/panhdjf/server_management_system/models"
+)
+
 // import (
 // 	"encoding/json"
 // 	"fmt"
@@ -8,52 +20,40 @@ package main
 // 	"net/http"
 // 	"os"
 // 	"strings"
-
-// 	"github.com/panhdjf/server_management_system/models"
 // )
 
-// // import (
-// // 	"encoding/json"
-// // 	"fmt"
-// // 	"io/ioutil"
-// // 	"log"
-// // 	"net/http"
-// // 	"os"
-// // 	"strings"
-// // )
-
-// // type ServerStatus struct {
-// // 	UpdateTime string `json:"uptime"`
-// // 	Status     string `json:"status"`
-// // }
-
-// func main() {
-// 	ID := "127.0.0.1"
-// 	url := strings.Join([]string{"http://", ID, ":8000/status"}, "")
-// 	response, err := http.Get(url)
-// 	// response, err := http.Get("http://192.168.2.0:8000/status")
-
-// 	if err != nil {
-// 		fmt.Print(err.Error())
-// 		os.Exit(1)
-// 	}
-
-// 	responseData, err := ioutil.ReadAll(response.Body)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(string(responseData))
-
-// 	var responseServer models.ServerStatus
-// 	err1 := json.Unmarshal(responseData, &responseServer)
-// 	if err1 != nil {
-// 		log.Fatal(err1)
-// 	}
-
-// 	fmt.Println(responseServer.UpdateTime)
-// 	fmt.Println(responseServer.Status)
-
+// type ServerStatus struct {
+// 	UpdateTime string `json:"uptime"`
+// 	Status     string `json:"status"`
 // }
+
+func main() {
+	ID := "127.0.0.1"
+	url := strings.Join([]string{"http://", ID, ":8000/status"}, "")
+	response, err := http.Get(url)
+	// response, err := http.Get("http://192.168.2.0:8000/status")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+
+	var responseServer models.ServerStatus
+	err1 := json.Unmarshal(responseData, &responseServer)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	fmt.Println(responseServer.UpdateTime)
+	fmt.Println(responseServer.Status)
+
+}
 
 // // func (sc ServerController) CheckStatusServer() (int, int, int, float64) {
 
